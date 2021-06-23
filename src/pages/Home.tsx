cbbe2826cd8b0981f0f0ee87ecd52f91d0117364
Malu <maahwf@gmail.com>
2021-06-23 00:28:35 -0300

    third commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router-dom'
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
@@ -5,8 +7,21 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
+    const history = useHistory();
+    const { user, signInWithGoogle } = useAuth()
+
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle()
+        }
+
+
+        history.push('/rooms/new');
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -17,7 +32,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
